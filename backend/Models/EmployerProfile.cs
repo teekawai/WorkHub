3466754e6250e5f9badbb372c8f289917bbd0c2d
teekawai <mkoolguy2005@gmail.com>
2026-08-20 21:51:23 +0700

    setup Backend

---
+++ backend/Models/EmployerProfile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace backend.Models;
+
+public partial class EmployerProfile
+{
+    public string UserId { get; set; } = null!;
+
+    public string CompanyName { get; set; } = null!;
+
+    public string? Logo { get; set; }
+
+    public string Address { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public string? Website { get; set; }
+
+    public string Field { get; set; } = null!;
+
+    public int? FoundedYear { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+
+    public string TaxCode { get; set; } = null!;
+
+    public virtual ICollection<Job> Jobs { get; set; } = new List<Job>();
+
+    public virtual User User { get; set; } = null!;
+}
